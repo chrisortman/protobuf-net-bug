c8a261bfd66abaac2476e217318b62edd6e64c7b
Chris Ortman <chriso@innovsys.com>
2012-08-14 09:23:50 -0500

    Example of deserializer bug
    Deserializing the object in protostream.bin works in v1, v2.0.0.480 but
    not v2.0.0.565
    
    the .proto and generated C# file are created using the visual studio
    extension which targets protobuf-net v1

---
+++ Deserialize.V1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using CmdlinkFileTransfer;
+using ProtoBuf;
+
+namespace Deserialize.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var slnDirectory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent;
+            
+            using(var fs = File.OpenRead(Path.Combine(slnDirectory.FullName,"protostream.bin")))
+            {
+                var deserialized = Serializer.Deserialize<GetDirectoryResponse>(fs);
+                
+            }
+            Console.WriteLine("V1 -- Success");
+        }
+    }
+}
---
+++ Deserialize.V2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using CmdlinkFileTransfer;
+using ProtoBuf;
+
+namespace Deserialize.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var slnDirectory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent;
+
+            try
+            {
+                using(var fs = File.OpenRead(Path.Combine(slnDirectory.FullName, "protostream.bin")))
+                {
+                    var deserialized = Serializer.Deserialize<GetDirectoryResponse>(fs);
+
+                }
+                Console.WriteLine("V2.0.0.480 -- Success");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("V2.0.0.480 -- ERROR");
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
